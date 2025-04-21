05d64a4a4da54b26e9d84592172ddf61a40731fd
youssef336 <youssefbelal2004@gmail.com>
2025-04-21 16:58:06 +0200

    feat: update AuthRepo to return Either type for user creation; add Failure class for error handling

--- lib/features/auth/domains/repos/auth_repo.dart
+++ lib/features/auth/domains/repos/auth_repo.dart
@@ -1,7 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub_app/core/errors/failures.dart';
 import 'package:fruits_hub_app/features/auth/domains/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
     String email,
     String password,
   );
