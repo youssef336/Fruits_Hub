2c67e485e2e8232a35378f64f48f0e3917770331
youssef336 <youssefbelal2004@gmail.com>
2025-06-03 19:30:07 +0300

    refactor

--- lib/core/widgets/custom_divider.dart
+++ lib/core/widgets/custom_divider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CustomDivider extends StatelessWidget {
   const CustomDivider({super.key});
--- lib/features/home/presentation/views/widgets/cart_view_body.dart
+++ lib/features/home/presentation/views/widgets/cart_view_body.dart
@@ -1,19 +1,14 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_app/constant.dart';
 import 'package:fruits_hub_app/core/widgets/build_app_bar.dart';
-import 'package:fruits_hub_app/core/widgets/custom_buttom.dart';
 import 'package:fruits_hub_app/features/home/presentation/manager/cubits/cart/cart_cubit.dart';
-import 'package:fruits_hub_app/features/home/presentation/views/widgets/cart_item.dart';
 
 import 'package:fruits_hub_app/features/home/presentation/views/widgets/cart_item_list.dart';
 import 'package:fruits_hub_app/features/home/presentation/views/widgets/cart_view_header.dart';
 import 'package:fruits_hub_app/main.dart';
 
 import '../../../../../core/widgets/custom_divider.dart';
-import '../../manager/cubits/cart_item/cart_item_cubit.dart';
 import 'custom_cart_buttom_bloc_builder.dart';
 
 class CartViewBody extends StatelessWidget {
